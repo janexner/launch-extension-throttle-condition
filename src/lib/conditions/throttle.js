f9a0024c78a4f7fb1b6e9659039b5222f7fa1aa3
Jan Exner <jexner@adobe.com>
2018-11-17 23:06:46 +0100

    in which our hero adds simple throttling by time

--- src/lib/conditions/throttle.js
+++ src/lib/conditions/throttle.js
@@ -1,5 +1,49 @@
 'use strict';
 
+const storageELementName = 'jeThrottleState';
+
 module.exports = function(settings, event) {
-  // TODO Return whether the condition passes.
+  var result = true; // passes by default
+
+  // get state
+  var oldStateRaw = sessionStorage.getItem(storageELementName);
+  oldStateRaw = oldStateRaw || "{}";
+  var oldState = JSON.parse(oldStateRaw);
+
+  // check max events per time
+  var timeUnit = settings.timeUnit;
+  var timeLimit = settings.timeLimit;
+  // convert timeLimit to seconds
+  switch (timeUnit) {
+    case 'days':
+      timeLimit = timeLimit * 60 * 60 * 24;
+      break;
+    case 'hours':
+      timeLimit = timeLimit * 60 * 60;
+      break;
+    case 'minutes':
+      timeLimit = timeLimit * 60;
+      break;
+    case 'seconds':
+    default:
+      break;
+  }
+  // how long ago was the last call? In seconds?
+  var difference = new Date();
+  difference = difference - oldState.lastCall;
+  difference = difference / 1000; // convert from ms to s
+  if (difference < timeLimit) {
+    // too early
+    result = false;
+  }
+
+  // done. store new state
+  var newState = oldState;
+  if (result) {
+    // since we'll reply treu, there will be a call. Record now as the time.
+    // (yes, technically, we're a bit too early)
+    newState.lastCall = new Date();
+  }
+  var newStateRaw = JSON.stringify(newState);
+  sessionStorage.setItem(storageELementName);
 };
